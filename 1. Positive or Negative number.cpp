8a5470dd3370c859340cae7ef9c3f99db4c4330d
Saurabh Tiwari <saurabh292000@gmail.com>
2023-10-06 21:05:03 +0530

    1. Positive or Negative number.cpp

--- 1. Positive or Negative number.cpp
+++ 1. Positive or Negative number.cpp
@@ -1,3 +1,12 @@
+// Check if a Number is Positive or Negative in C++
+// Given an integer input, The objective is to write a code to Check if a Number is Positive or Negative in C++ Language.
+
+// For Instance,
+// Input : num = 10
+// Output :The number is Positive
+
+
+
 #include<iostream>
 using namespace std;
 int main()
